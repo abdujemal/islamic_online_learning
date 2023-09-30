7e09d5107853be16cfc3f0a9ca898e52541a42a1
Abdu Jemal <abdu1994jemal@gmail.com>
2023-09-30 17:01:14 +0300

    just removed auth feature

--- test/features/main/data/main_data_src_test.dart
+++ test/features/main/data/main_data_src_test.dart
@@ -1,18 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-// import 'package:firebase_database/firebase_database.dart';
-// import 'package:firebase_database_mocks/firebase_database_mocks.dart'; //firebase_database_mocks
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:islamic_online_learning/core/failure.dart';
 import 'package:islamic_online_learning/features/main/data/Model/course_model.dart';
 import 'package:islamic_online_learning/features/main/data/main_data_src.dart';
 import 'package:mockito/mockito.dart';
 
 import '../../../teat_helper.mocks.dart';
-
-// import '../../../teat_helper.mocks.dart';
-
-// import '../../../teat_helper.mocks.dart';
 
 void main() {
   late MFirebaseDatabase mockFirebaseDatabase;
@@ -63,30 +55,6 @@ void main() {
     }
   };
 
-  // mockFirebaseDatabase.ref().child("Courses").set(
-  // {
-  //   '1': {
-  //     "author": "author",
-  //     'category': "category",
-  //     'courseIds': "courseIds",
-  //     'noOfRecord': 10,
-  //     'pdfId': "pdfId",
-  //     'title': "title",
-  //     'ustaz': "ustaz",
-  //   },
-  //   '2': {
-  //     'author': "author1",
-  //     'category': "category1",
-  //     'courseIds': "courseIds1",
-  //     'noOfRecord': 20,
-  //     'pdfId': "pdfId1",
-  //     'title': "title1",
-  //     'ustaz': "ustaz1",
-  //   }
-  // }
-  // );
-
-  Exception exception = Exception("errorrr occured.");
   setUp(() {
     mockFirebaseDatabase = MFirebaseDatabase();
     mockDatabaseReference = MockDatabaseReference();
@@ -109,28 +77,5 @@ void main() {
       expect(result, equals(courses));
     });
 
-    test("should throw an error if it is not successful.", () async {
-      // arrange
-      // final mockReference = MockDatabaseReference();
-      // final mockChildReference = MockDatabaseReference();
-      // when(mockFirebaseDatabase.ref()).thenReturn(mockReference);
-      // when(mockReference.child("Courses")).thenReturn(mockChildReference);
-
-      when(mockFirebaseDatabase.ref('Courses')).thenReturn(
-          mockDatabaseReference); // Stub get() method on child reference
-      when(mockDatabaseReference.get()).thenAnswer((_) => throw exception);
-
-      // act
-      final result = await mainDataSrc.getCourses(page);
-
-      // assert
-      expect(result, exception);
-    });
   });
 }
-
-
-// type 'Null' is not a subtype of type 'DatabaseReference'
-// test/features/main/data/main_data_src_test.dart 13:7    MockDatabaseReference.child
-// test/features/main/data/main_data_src_test.dart 102:26  main.<fn>.<fn>
-
